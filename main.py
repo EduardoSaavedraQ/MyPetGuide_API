3832c1a57a3492cb27bfbeeb686832919f464160
EduardoSaavedraQ <eduardoooo89@hotmail.com>
2025-08-22 17:49:35 -0600

    Commit temporal para probar conexión a base de datos

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from sqlmodel import create_engine, text
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+
+load_dotenv()
 
 @app.get('/')
 def index() -> dict:
@@ -9,3 +14,23 @@ def index() -> dict:
 @app.get('/health')
 def health() -> dict:
     return {'status': "ok"}
+
+
+@app.get('/test-database-connection')
+def test_db_conn() -> dict:
+    USER = os.getenv("DB_USERNAME")
+    PASSWORD = os.getenv("DB_PASSWORD")
+    HOST = os.getenv("DB_HOST")
+    PORT = os.getenv("DB_PORT")
+    DBNAME = os.getenv("DB_DATABASE")
+
+    # Construct the SQLAlchemy connection string
+    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+    engine = create_engine(DATABASE_URL)
+
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT version()"))
+        version = result.scalar()
+        return {"version": version}
+
