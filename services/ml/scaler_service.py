bb3fad5d1bcfbd3a9af59d77aa95c278c290c045
EduardoSaavedraQ <eduardoooo89@hotmail.com>
2025-11-13 21:34:10 -0600

    Añadir docstrings a servicio scaler_service

--- services/ml/scaler_service.py
+++ services/ml/scaler_service.py
@@ -4,12 +4,25 @@ import os
 SCALERS_PATH = os.path.join(os.path.dirname(__file__), "../../artifacts/Scalers/")
 
 def load_scaler(scaler_type: str):
+    """Carga el escalador especificado en `scaler_type`.
+    Valores válidos: 'owner' y 'pet'.
+    """
     if scaler_type not in {"owner", "pet"}:
         raise ValueError("Invalid scaler type. Choose 'owner' or 'pet'.")
     
     return joblib.load(os.path.join(SCALERS_PATH, f"{scaler_type}_scaler.pkl"))
 
-def scale_data(scaler_type: str, data: list):
+def scale_data(scaler_type: str, data: list) -> list:
+    """Escala los valores recibidos en `data` según el tipo especificado.
+
+    Args:
+        scaler_type (str): Tipo de escalador que se desea usar.
+        data (list): Una lista que contiene los valores de un registro o varios registros con valores que se desean escalar.
+
+    Returns:
+        list: Valores escalados.
+    """
+
     # Si data es una lista plana, la convertimos a lista de listas
     if isinstance(data, list) and all(isinstance(x, (int, float)) for x in data):
         data_to_scale = [data]
