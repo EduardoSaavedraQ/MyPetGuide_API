1963c22075fa7cf135badbd85de0420b72f65f11
EduardoSaavedraQ <eduardoooo89@hotmail.com>
2025-11-13 21:33:06 -0600

    Corrección de función transfer_pet_ownership en migraciones de Supabase

---
+++ supabase/migrations/20251112230842_correct_function_transfer_pet_ownership.sql
@@ -0,0 +1,19 @@
+Create or replace function transfer_pet_ownership(
+    pet_id BIGINT,
+    old_owner UUID,
+    new_owner UUID
+) RETURNS void as $$
+BEGIN
+    INSERT INTO addoptions(id_pet, id_prev_owner, id_new_owner)
+    VALUES (pet_id, old_owner, new_owner);
+
+    UPDATE pets
+    SET id_owner = new_owner,
+        in_adoption_process = FALSE
+    WHERE id_pet = pet_id;
+    
+    UPDATE chat_rooms
+    SET finished = true
+    WHERE id_pet = pet_id;
+END;
+$$ language plpgsql security definer;
