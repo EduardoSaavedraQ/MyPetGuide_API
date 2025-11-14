f0d514007f36290c0c3fa68a977626176addd629
EduardoSaavedraQ <eduardoooo89@hotmail.com>
2025-11-13 21:17:49 -0600

    Corregir límite inferior de campo vailable_time_per_day en esquema UserProfileCreate

--- schemas/users.py
+++ schemas/users.py
@@ -130,7 +130,7 @@ class UserProfileCreate(SQLModel):
     activity_level: int | None = Field(default=None, ge=1, le=3, description="Nivel de activiad del usuario o el responsable directo de la mascota")
     has_kids: bool | None = Field(default=None, description="Indica si hay niños en la familia")
     has_neighbors: bool | None = Field(default=None, description="Indica si hay vecinos cerca")
-    available_time_per_day: int | None = Field(default=None, ge=2, le=24, description="Tiempo disponible al día para cuidar a la mascota (en horas, máximo 24)")
+    available_time_per_day: int | None = Field(default=None, ge=1, le=24, description="Tiempo disponible al día para cuidar a la mascota (en horas, máximo 24)")
     vet_access: bool | None = Field(default=None, description="Indica si se tiene acceso a un veterinario")
     has_other_pets: bool | None = Field(default=None, description="Indica si ya se tienen otras mascotas")
     experience_with_pets: int | None = Field(default=None, description="Tiempo experiencia cuidando mascotas")
